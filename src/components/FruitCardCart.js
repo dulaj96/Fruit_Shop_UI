6b299748947c4c1bf5ff5ce67b19dfa2330c20f3
dulaj96 <dulajmithun96@gmail.com>
2023-10-28 19:52:20 +0530

    create cart screen

---
+++ src/components/FruitCardCart.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FruitCardCart = () => {
+  return (
+    <View>
+      <Text className='text-red-800'>FruitCardCart</Text>
+    </View>
+  )
+}
+
+export default FruitCardCart
--- src/screens/CartScreen.js
+++ src/screens/CartScreen.js
@@ -1,11 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView, StatusBar, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { useNavigation } from '@react-navigation/native'
+import Icon from 'react-native-vector-icons/Ionicons';
+import { themeColors } from '../theme/Index';
+import { cartItems } from '../constants/Index';
+import FruitCardCart from '../components/FruitCardCart';
 
 const CartScreen = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>CartScreen</Text>
-    </View>
+    <SafeAreaView className='flex-1 flex  bg-white'>
+      <StatusBar backgroundColor={'white'} barStyle='dark-content' />
+      <View className='flex-row justify-start'>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          className='mx-4 my-3 border border-gray-400 rounded-xl '
+          style={{ backgroundColor: 'rgba(255,255,255,0.2)' }}
+        >
+          <Icon name='chevron-back' size={30} color={'gray'} />
+        </TouchableOpacity>
+      </View>
+      <View className='mx-4'>
+        <Text 
+        style={{color: themeColors.text}}
+        className='py-10 text-2xl'
+        >
+          Your <Text className='font-bold'>Cart</Text>
+        </Text>
+        <View>
+          {
+            cartItems.map((fruit, index) => {
+              return(
+                <FruitCardCart fruit={fruit} key={index}/>
+              )
+            })
+          }
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
